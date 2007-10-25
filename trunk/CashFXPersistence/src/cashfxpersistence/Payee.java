8eb8e29704918d5741b99c05b9997ab24b3f08ba
wsnyder6 <wsnyder6@cdafb44a-e119-0410-bfc3-317ced75d77f>
2007-10-25 03:27:10 +0000

    initial add

---
+++ trunk/CashFXPersistence/src/cashfxpersistence/Payee.java
@@ -0,0 +1,87 @@
+/*
+ * Payee.java
+ * 
+ * Created on Oct 23, 2007, 11:15:16 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cashfxpersistence;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author TWXS025
+ */
+@Entity
+@Table(name = "PAYEE")
+@NamedQueries({@NamedQuery(name = "Payee.findById", query = "SELECT p FROM Payee p WHERE p.id = :id"), @NamedQuery(name = "Payee.findByName", query = "SELECT p FROM Payee p WHERE p.name = :name")})
+public class Payee implements Serializable {
+    @Id
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    public Payee() {
+    }
+
+    public Payee(Integer id) {
+        this.id = id;
+    }
+
+    public Payee(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Payee)) {
+            return false;
+        }
+        Payee other = (Payee) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cashfxpersistence.Payee[id=" + id + "]";
+    }
+
+}
---
+++ trunk/CashFXPersistence/src/cashfxpersistence/Payment.java
@@ -0,0 +1,136 @@
+/*
+ * Payment.java
+ * 
+ * Created on Oct 23, 2007, 11:15:16 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cashfxpersistence;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author TWXS025
+ */
+@Entity
+@Table(name = "PAYMENT")
+@NamedQueries({@NamedQuery(name = "Payment.findById", query = "SELECT p FROM Payment p WHERE p.id = :id"), @NamedQuery(name = "Payment.findByPayeeId", query = "SELECT p FROM Payment p WHERE p.payeeId = :payeeId"), @NamedQuery(name = "Payment.findByAmount", query = "SELECT p FROM Payment p WHERE p.amount = :amount"), @NamedQuery(name = "Payment.findByStartDate", query = "SELECT p FROM Payment p WHERE p.startDate = :startDate"), @NamedQuery(name = "Payment.findByEndDate", query = "SELECT p FROM Payment p WHERE p.endDate = :endDate"), @NamedQuery(name = "Payment.findByOccurence", query = "SELECT p FROM Payment p WHERE p.occurence = :occurence")})
+public class Payment implements Serializable {
+    @Id
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+    @Column(name = "PAYEE_ID", nullable = false)
+    private int payeeId;
+    @Column(name = "AMOUNT", nullable = false)
+    private double amount;
+    @Column(name = "START_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+    @Column(name = "END_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+    @Column(name = "OCCURENCE", nullable = false)
+    private String occurence;
+
+    public Payment() {
+    }
+
+    public Payment(Integer id) {
+        this.id = id;
+    }
+
+    public Payment(Integer id, int payeeId, double amount, Date startDate, Date endDate, String occurence) {
+        this.id = id;
+        this.payeeId = payeeId;
+        this.amount = amount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.occurence = occurence;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getPayeeId() {
+        return payeeId;
+    }
+
+    public void setPayeeId(int payeeId) {
+        this.payeeId = payeeId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getOccurence() {
+        return occurence;
+    }
+
+    public void setOccurence(String occurence) {
+        this.occurence = occurence;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Payment)) {
+            return false;
+        }
+        Payment other = (Payment) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cashfxpersistence.Payment[id=" + id + "]";
+    }
+
+}
